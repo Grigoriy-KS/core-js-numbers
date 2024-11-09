c73495ff51eddb6345889ebd89128a1b1390e319
Grigoriy Ksyonz <KsyonzGS@gmail.com>
2024-11-09 14:03:25 +0300

    feat: create a getSumOfDigits function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -279,8 +279,11 @@ function getSumToN(n) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  return num
+    .toString(10)
+    .split('')
+    .reduce((accumulator, currentValue) => +accumulator + +currentValue);
 }
 
 /**
